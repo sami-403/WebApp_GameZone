df47df8dd9d1f45ed13f643a519def4d4c209573
devRafaelFagundes <DevRafaelFagundes@gmail.com>
2025-12-06 10:39:10 -0300

    see more funcionando

--- scripts/lancamentosGameCards.js
+++ scripts/lancamentosGameCards.js
@@ -1,6 +1,7 @@
 import { games } from "./lancamentosGameData.js";
 import { addFavorite } from "./addfavorites.js";
 
+const body = document.querySelector('body')
 const gameCardsContainer = document.querySelector('.gameCardsContainer')
 let addId = 10;
 function renderCards(games, container) {
@@ -9,17 +10,32 @@ function renderCards(games, container) {
         const cardContainer = document.createElement('div')
         cardContainer.style.position = "relative"
         cardContainer.innerHTML = `
-            <img src="${game.path}" class="gameCard" style="display: block; height:100%;"></img>
-            <button class="favbutton" data-id="${addId}" data-img="${game.path}"></button>
+            <img src="${game.path}" class="gameCard" style="display: block; height:100%;"  data-id="${addId}" data-img="${game.path}"></img>
+            <button class="favbutton"></button>
         `
-        cardContainer.style.backgroundImage = `url(${game.path})`
-
         const seeMoreButton = document.createElement('button')
         seeMoreButton.innerText = 'Veja mais'
         seeMoreButton.classList.add('seeMoreButton')
         seeMoreButton.addEventListener('click', (event) => {
             removeOverlay();
             createOverlay();
+            const seeMoreCard = document.createElement('div')
+            seeMoreCard.classList.add("seeMoreCard");
+
+            seeMoreCard.innerHTML = `
+                <h2 class="seeMoreCardTitle">${game.title}</h2>
+                <p class="seeMoreCardDeveloper"><b>Developer:</b> ${game.developer}</p>
+                <p class="seeMoreCardYear"><b>Ano:</b> ${game.releaseYear}</p>
+                <p class="seeMoreCardPlatforms"><b>Plataformas:</b> 
+                    ${game.platforms.join(", ")}
+                </p>
+                <p class="seeMoreCardAbout">${game.about}</p>
+                <button class="seeMoreCloseBtn">Entendi</button>
+            `;
+
+            document.querySelector('.overlayScreen').append(seeMoreCard);
+            seeMoreCard.querySelector(".seeMoreCloseBtn").addEventListener("click", removeOverlay)
+
         })
 
         cardContainer.append(seeMoreButton)
@@ -32,6 +48,7 @@ function renderCards(games, container) {
 function createOverlay() {
     const overlayContainer = document.createElement('div')
     overlayContainer.classList.add('overlayScreen')
+    body.append(overlayContainer)    
 }
 
 function removeOverlay() {

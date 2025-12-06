d34247145e49dae42ad1d9aad3ce19a7317d87ca
devRafaelFagundes <DevRafaelFagundes@gmail.com>
2025-12-06 08:52:13 -0300

    cartas renderizadas e adicao de botoes de favoritar e ver mais

--- scripts/lancamentosGameCards.js
+++ scripts/lancamentosGameCards.js
@@ -1,24 +1,31 @@
 import { games } from "./lancamentosGameData.js";
+import { addFavorite } from "./addfavorites.js";
 
 const gameCardsContainer = document.querySelector('.gameCardsContainer')
-
-function renderCards(games) {
+let addId = 10;
+function renderCards(games, container) {
     const sortedGames = games.sort((a, b) => a.releaseYear - b.releaseYear)
     sortedGames.forEach(game => {
         const cardContainer = document.createElement('div')
+        cardContainer.style.position = "relative"
+        cardContainer.innerHTML = `
+            <img src="${game.path}" class="gameCard" style="display: block; height:100%;"></img>
+            <button class="favbutton" data-id="${addId}" data-img="${game.path}"></button>
+        `
         cardContainer.style.backgroundImage = `url(${game.path})`
 
-        // const favoriteButton = document.createElement('button');
-        // favoriteButton.innerText = 'Favoritar'
-        // favoriteButton.onclick = `${() => {
-        //     //favorite
-        //     //funcao de Allan
-        // }}`
-
         const seeMoreButton = document.createElement('button')
+        seeMoreButton.innerText = 'Veja mais'
+        seeMoreButton.classList.add('seeMoreButton')
         seeMoreButton.addEventListener('click', (event) => {
-            
+            removeOverlay();
+            createOverlay();
         })
+
+        cardContainer.append(seeMoreButton)
+
+        container.append(cardContainer);
+        ++addId;
     })
 }
 
@@ -29,7 +36,12 @@ function createOverlay() {
 
 function removeOverlay() {
     const overlays = document.querySelectorAll('.overlayScreen')
-    overlays.forEach(over => {
-        over.remove()
-    })
+    if(overlays.length > 0) {
+        overlays.forEach(over => {
+            over.remove()
+        })
+    }
 }
+
+renderCards(games, gameCardsContainer);
+addFavorite()
